0354fb677133a14b13f6b392f0cc1d94852a986e
Atiqur Rahman <atiqurrahman2312002@gmail.com>
2024-07-14 20:55:09 +0600

    update

---
+++ DAL/Interfaces/BrideRepo.cs
@@ -0,0 +1,45 @@
+﻿using DAL.Data;
+using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public class BrideRepo : IBrideRepo
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public BrideRepo(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public Task<Bride> AddBrideAsync(Bride bride)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Bride> DeleteBrideAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Bride>> GetAllBridesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Bride> GetBrideByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Bride> UpdateBrideAsync(Bride bride)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DAL/Interfaces/IBrideRepo.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IBrideRepo
+    {
+        Task<IEnumerable<Bride>> GetAllBridesAsync();
+        Task<Bride> GetBrideByIdAsync(int id);
+        Task<Bride> AddBrideAsync(Bride bride);
+        Task<Bride> UpdateBrideAsync(Bride bride);
+        Task<Bride> DeleteBrideAsync(int id);
+
+    }
+}
